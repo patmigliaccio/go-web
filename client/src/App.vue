006193b5efadf398f24316fe06faa1331eba39aa
Pat Migliaccio <pat@patmigliaccio.com>
2017-04-02 10:06:05 -0400

    init vue webapp. init webpack

---
+++ client/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h2>Go Web Application.</h2>
+    <router-view></router-view>
+    <div class="footer">
+      Created by <a href="https://github.com/patmigliaccio">@patmigliaccio</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+a {
+	font-size: 1em;
+	font-weight: 600;
+	color: #0072c6;
+	text-decoration: none;
+}
+
+a:hover {
+	color: #999;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <p>{{ msg }}</p>
+    <input type="text" v-model="id" />
+    <button @click="getTune">GET Tune</button>
+    <div v-html="tune"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data: function () {
+    return {
+      msg: 'A work in progress.',
+      id: '840f77d0-4f12-49c7-a803-551641bcda5d'
+    }
+  },
+  methods: {
+    getTune: function () {
+      alert(this.id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
